3578ca94ac9a22e11dd930ca98220c8edc9dc5f1
Óskar Eiðsson <ose1975@gmail.com>
2024-09-20 17:50:33 +0000

    First commit

--- C_sharp_primer/Program.cs
+++ C_sharp_primer/Program.cs
@@ -1,2 +1,66 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> numbers = new List<int>();
+        string input;
+
+        Console.WriteLine("Enter a number (or press Enter to finish):");
+
+        while (true)
+        {
+            input = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(input))
+            {
+                break;
+            }
+
+            if (int.TryParse(input, out int number))
+            {
+                numbers.Add(number);
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid number.");
+            }
+        }
+
+        if (numbers.Count > 0)
+        {
+            int max = numbers[0];
+            int min = numbers[0];
+
+            foreach (int num in numbers)
+            {
+                if (num > max)
+                {
+                    max = num;
+                }
+
+                if (num < min)
+                {
+                    min = num;
+                }
+            }
+
+            Console.WriteLine($"Largest number: {max}");
+            Console.WriteLine($"Smallest number: {min}");
+        }
+        else
+        {
+            Console.WriteLine("No numbers were entered.");
+        }
+
+
+    }
+}
+
+
+
+
+
